1bb9bad43234236c1e8f29f619a246cd94a72fdc
Sam Donohoo <100438250+SamDonohoo@users.noreply.github.com>
2025-07-30 14:14:53 -0500

    Update Fstat_Calculation.r
    Amended to include Significance Testing.

--- Fstat_Calculation.r
+++ Fstat_Calculation.r
@@ -14,7 +14,7 @@ genepops<-list.files(pattern="*.genepop")
 gens<-gsub(".genepop$",".gen", genepops)
 file.rename(genepops,gens)
 
-####Uncomment if you want to calculate Weir and Cockerham Fst and use that.
+## Calculate Weir and Cockerham Fst
 dat<-read.genepop("Elimia_R80_maf025_Multi.haps.gen")##Read in Genepop
 data2<-genind2hierfstat(dat)
 data2$pop #Sanity check to make sure populations are accurately defined.
@@ -23,3 +23,19 @@ WC[is.na(WC)]<-0
 write.table(WC,file="WC_FST.csv",row.names=TRUE, col.names = NA, sep = ",")
 WC<-as.dist(WC)
 WC
+
+## Run Bootstrapping to Evaluate Significance
+# P-value of 0.05
+Boot <- boot.ppfst(dat=data2,nboot=1000,quant=c(0.025,0.975),diploid=TRUE)
+Boot[is.na(Boot)]<-0
+print(Boot)
+
+# P-value 0f 0.01
+Boot.01 <- boot.ppfst(dat=data2,nboot=1000,quant=c(0.005,0.995),diploid=TRUE)
+Boot.01[is.na(Boot.01)]<-0
+print(Boot.01)
+
+# P-value of 0.001
+Boot.001 <- boot.ppfst(dat=data2,nboot=1000,quant=c(0.0005,0.9995),diploid=TRUE)
+Boot.001[is.na(Boot.001)]<-0
+print(Boot.001)
